bbc5b2ed7be4731b49487b1e713c2d3e036465f9
Joshua Oakes <joshuaoakes@hotmail.com>
2023-06-30 21:50:32 -0700

    Move to separate file

--- ScratchConsole/FluentHttp/Result/RestUnitResult.cs
+++ ScratchConsole/FluentHttp/Result/RestUnitResult.cs
@@ -1,24 +1,5 @@
 ﻿namespace ScratchConsole.FluentHttp.Result;
 
-internal readonly struct RestUnitResult<P> : IRestUnitResult<P>
-{
-    public bool IsSuccess { get; }
-
-    public bool IsFailure => !IsSuccess;
-
-    public HttpResponseMessage ResponseMessage { get; }
-
-    private readonly P _problem;
-    public P Problem => IsFailure ? _problem : throw new Exception(); // TODO subtype exception
-
-    internal RestUnitResult(bool isSuccess, P problem, HttpResponseMessage responseMessage)
-    {
-        IsSuccess = isSuccess;
-        _problem = problem;
-        ResponseMessage = responseMessage;
-    }
-}
-
 public static class RestUnitResult
 {
     internal static RestUnitResult<P> Success<P>(HttpResponseMessage responseMessage) => new(true, default!, responseMessage);
---
+++ ScratchConsole/FluentHttp/Result/RestUnitResultP.cs
@@ -0,0 +1,20 @@
+﻿namespace ScratchConsole.FluentHttp.Result;
+
+internal readonly struct RestUnitResult<P> : IRestUnitResult<P>
+{
+    public bool IsSuccess { get; }
+
+    public bool IsFailure => !IsSuccess;
+
+    public HttpResponseMessage ResponseMessage { get; }
+
+    private readonly P _problem;
+    public P Problem => IsFailure ? _problem : throw new Exception(); // TODO subtype exception
+
+    internal RestUnitResult(bool isSuccess, P problem, HttpResponseMessage responseMessage)
+    {
+        IsSuccess = isSuccess;
+        _problem = problem;
+        ResponseMessage = responseMessage;
+    }
+}
